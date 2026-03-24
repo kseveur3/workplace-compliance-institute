0ab297ac3364dace02a8c9fabc39447f407dfbd5
kevin seveur <kevsev@Mac.lan>
2026-03-24 12:53:27 -0400

    Add Express server for Heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const distPath = path.join(__dirname, "dist");
+
+app.use(express.static(distPath));
+
+app.use((_req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
